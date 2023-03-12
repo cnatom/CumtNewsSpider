6b3d5f5bd2c3f4b90316e1b0e16ff78880516e4c
CUMT-Atom <cnatom@foxmail.com>
2023-03-12 12:26:53 +0800

    🐛bugfix(app):增强爬虫兼容性

--- app.py
+++ app.py
@@ -6,16 +6,31 @@ app = Flask(__name__)
 
 @app.route('/news/list', methods=['GET'])
 def getNews():
-    type = request.args.get("type", "SDXW", type=str)
-    page = request.args.get("page", 1, type=int)
-    result = spider.NewsSpider(type, page).toJson()
-    return jsonify(result)
+    try:
+        type = request.args.get("type", "SDXW", type=str)
+        page = request.args.get("page", 1, type=int)
+        result = spider.NewsSpider(type, page).toJson()
+        return jsonify(result)
+    except Exception as e:
+        print(e)
+        return jsonify(
+            {
+                "current_page": 1,
+                "max_page": 1,
+                "type": "",
+                "data": {
+                    "title": "解析失败",
+                    "link": "",
+                    "date": ""
+                }
+            }
+        )
 
 
 @app.route("/news/type", methods=['GET'])
 def getTypeList():
     typeList = []
-    for key, value in spider.urlTypeMap.items():
+    for key, value in spider.typeMap.items():
         typeList.append({
             "type": key,
             "name": value.name,
@@ -29,9 +44,13 @@ def getTypeList():
 
 @app.route('/news/content', methods=['GET'])
 def getNewsDetail():
-    link = request.args.get("link", type=str)
-    result = spider.ContentSpider(link).toJson()
-    return jsonify(result)
+    try:
+        link = request.args.get("link", type=str)
+        result = spider.ContentSpider(link).toJson()
+        return jsonify(result)
+    except Exception as e:
+        print(e)
+        return jsonify(spider.NewsContent("解析失败", "", "", [], 0))
 
 
 if __name__ == '__main__':
--- spider.py
+++ spider.py
@@ -3,12 +3,14 @@ from bs4 import BeautifulSoup
 import re
 
 
+# 新闻类型
 class NewsType:
     def __init__(self, name, link):
         self.name = name
         self.link = link
 
-urlTypeMap = {
+
+typeMap = {
     "SDXW": NewsType("视点新闻", "https://www.cumt.edu.cn/19673"),
     "XSJJ": NewsType("学术聚焦", "https://www.cumt.edu.cn/19674"),
     "XSBG": NewsType("学术报告", "https://www.cumt.edu.cn/19676"),
@@ -18,6 +20,7 @@ urlTypeMap = {
 }
 
 
+# 新闻列表项
 class News:
     def __init__(self, title, link, time):
         self.title = title
@@ -32,12 +35,13 @@ class News:
         }
 
 
+# 新闻列表爬虫
 class NewsSpider:
     header = {
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"}
 
     def __init__(self, typeStr, page=1):
-        self.type = urlTypeMap[typeStr]
+        self.type = typeMap[typeStr]
         self.url = self.type.link
         self.page = page
         if self.page != 1:
@@ -65,7 +69,7 @@ class NewsSpider:
         soup = soup[0].select('li')
         for i in soup:
             title = i.select('a')[0].get_text()
-            link = i.select('a')[0].get('href')
+            link = self.__genLink(i.select('a')[0].get('href'))
             time = i.select('span[class="news_meta"]')[0].get_text()
             news = News(title, link, time)
             result.append(news.toJson())
@@ -83,8 +87,15 @@ class NewsSpider:
         soup = soup.select('em[class="all_pages"]')
         return int(soup[0].get_text())
 
+    def __genLink(self, link):
+        if link.startswith("http"):
+            return link
+        else:
+            return "https://www.cumt.edu.cn" + link
 
-class ContentSection:
+
+# 新闻内容单个段落
+class ContentParagraph:
     def __init__(self, type, content):
         self.type = type
         self.content = content
@@ -93,6 +104,25 @@ class ContentSection:
         return {
             "type": self.type,
             "content": self.content
+        }
+
+
+# 新闻内容
+class NewsContent:
+    def __init__(self, title, author, date, contents, visitCount):
+        self.title = title
+        self.author = author
+        self.date = date
+        self.content = contents
+        self.visitCount = visitCount
+
+    def toJson(self):
+        return {
+            "title": self.title,
+            "author": self.author,
+            "date": self.date,
+            "contents": self.content,
+            "visit_count": self.visitCount
         }
 
 
@@ -112,26 +142,73 @@ class ContentSpider:
 
     def toJson(self):
         soup = BeautifulSoup(self.html, 'html.parser')
-        title = soup.select('h1[class="arti_title"]')[0].getText().strip()
-        temp = soup.select('div[class="news_xx"]')[0].findAll('span')
-        author = temp[1].getText()
-        date = temp[3].getText()
-        visitCount = self._getVisitCount(temp[5].find_next().get("url"))
-        contents = soup.select('div[class="wp_articlecontent"]')[0].findAll()
-        contentsResult = []
-        for content in contents:
-            if content.name == "p":
-                contentsResult.append(ContentSection("text", content.getText()).toJson())
-            elif content.name == "img":
-                contentsResult.append(ContentSection("image", self.urlHead[:-1] + content.get("src")).toJson())
-        result = {
-            "title": title,
-            "author": author,
-            "date": date,
-            "contents": contentsResult,
-            "visit_count": visitCount
-        }
+        result = NewsContent("", "", "", [], 0).toJson()
+        result = self.__normalPageToJson(soup)
+        if result is None:
+            result = self.__pdfPageToJson(soup)
         return result
+
+    def __normalPageToJson(self, soup):
+        try:
+            title = soup.select('h1[class="arti_title"]')[0].getText().strip()
+            temp = soup.select('div[class="news_xx"]')[0].findAll('span')
+            author = temp[1].getText()
+            date = temp[3].getText()
+            visitCount = self._getVisitCount(temp[5].find_next().get("url"))
+            contents = soup.select('div[class="wp_articlecontent"]')[0]
+            contentsResult = self.__analyticalParagraph(contents)
+            result = NewsContent(title, author, date, contentsResult, visitCount).toJson()
+            return result
+        except Exception as e:
+            print(e)
+            return None
+
+    # 解析pdf段落
+    def __analyticalPdfParagraph(self, soup):
+        pdfUrlTail = soup.get("pdfsrc")[1:]
+        pdfUrl = self.urlHead + pdfUrlTail
+        return ContentParagraph("pdf", pdfUrl).toJson()
+
+    # 平行遍历标签，解析每一段
+    def __analyticalParagraph(self, soup):
+        contentsResult = []
+        for content in soup.children:
+            try:
+                if content.name is None:
+                    continue
+                elif content.name == "p":
+                    if content.next.name == "img":
+                        contentsResult.append(
+                            ContentParagraph("image", self.urlHead[:-1] + content.next.get("src")).toJson())
+                    elif content.find_all('div'):
+                        contentsResult.append(
+                            self.__analyticalPdfParagraph(content.find_all('div')[0]))
+                    else:
+                        contentsResult.append(ContentParagraph("text", content.getText()).toJson())
+                elif content.name == "div":
+                    contentsResult.append(self.__analyticalPdfParagraph(content))
+
+                else:
+                    contentsResult.append(
+                        ContentParagraph("text", "未解析段落类型：" + content.name).toJson())
+            except Exception as e:
+                print(e)
+                contentsResult.append(ContentParagraph("text", "[该段落解析失败]").toJson())
+        return contentsResult
+
+    def __pdfPageToJson(self, soup):
+        try:
+            title = soup.select('h1[class="arti_title"]')[0].getText().strip()
+            author = soup.select('span[class="arti_publisher"]')[0].getText().split("：")[1]
+            date = soup.select('span[class="arti_update"]')[0].getText().split("：")[1]
+            visitCount = self._getVisitCount(soup.select('span[class="WP_VisitCount"]')[0].get("url"))
+            contents = soup.select('div[class="wp_articlecontent"]')[0]
+            contentsResult = self.__analyticalParagraph(contents)
+            result = NewsContent(title, author, date, contentsResult, visitCount).toJson()
+            return result
+        except Exception as e:
+            print(e)
+            return None
 
     def _getVisitCount(self, urlTail):
         url = self.urlHead + urlTail[1:]
